ee99f4aebd06faf0be557ce9f761916adaec4582
tnfo222 <tayforbes92@hotmail.com>
2022-06-05 13:32:08 -0400

    Added movement and borders to the character and its grid

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-const grid = document.querySelectorAll(".containerGame div")
-console.log(grid)
-let currentIndex = 76
+//select every div square in the grid
+const grid = document.querySelectorAll(".containerGame div");
+const gridWidth = 9;
 
-function movePlayableCharacter() {
+//show the grid array
+console.log(grid)
+
+//set the starting square by selecting a square in the array
+let currentIndex = 76;
+
+//move the Character with WASD and create borders
+function movePlayableCharacter(e) {
+    
+    grid[currentIndex].classList.remove('playableCharacter')
+
+    switch(e.keyCode) {
+        case 65:
+            if(currentIndex % gridWidth !== 0){
+                currentIndex -= 1;
+                console.log("moved left");
+            }
+            break;
+        case 87:
+            if(currentIndex - gridWidth >= 0){
+                currentIndex -= gridWidth;
+                console.log("moved up");
+            }
+            break;
+        case 68:
+            if(currentIndex % gridWidth < gridWidth - 1){
+                currentIndex += 1;
+                console.log("moved right")
+            }
+            break;
+        case 83:
+            if(currentIndex + gridWidth < gridWidth * gridWidth){
+                currentIndex += gridWidth;
+                console.log("moved down")
+            }
+            break;
+    }
     grid[currentIndex].classList.add('playableCharacter')
 }
-document.addEventListener('keypress', movePlayableCharacter)
+document.addEventListener('keydown', movePlayableCharacter)
